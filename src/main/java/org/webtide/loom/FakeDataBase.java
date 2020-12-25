bdc83d5ed5aea48496beab05ca24d1f86e6545e0
gregw <gregw@webtide.com>
2020-12-25 16:50:12 +0100

    WIP
    Testing threadpool vs virtual threads

---
+++ src/main/java/org/webtide/loom/FakeDataBase.java
@@ -0,0 +1,62 @@
+package org.webtide.loom;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.Stream;
+
+import org.openjdk.jmh.infra.Blackhole;
+
+public class FakeDataBase
+{
+    private static final Random RANDOM = new SecureRandom();
+    private static final Semaphore POOL = new Semaphore(100);
+    private static LongAdder result = new LongAdder();
+
+    public static Object get(String key) throws InterruptedException
+    {
+        // pretend to get a connection from a JDBC connection pool
+        POOL.acquire();
+
+        // pretend to talk to a remote server
+        Thread.sleep(10 + RANDOM.nextInt(10));
+
+        // pretend to get a result from the database
+        Object result = Long.toHexString(key.hashCode() + RANDOM.nextLong()).repeat(5 + RANDOM.nextInt(5));
+
+        // Consume some CPU with semaphore
+        Blackhole.consumeCPU(1000 + result.hashCode() % 1000);
+
+        // Put our thread back into the database.
+        POOL.release();
+
+        return result;
+    }
+
+    public static long getResult()
+    {
+        return result.longValue();
+    }
+
+    public static void put(String key, Object value) throws InterruptedException
+    {
+        // pretend to marshal the update to the database
+        long data = Stream.of(value.toString().toCharArray()).count();
+
+        // pretend to get a connection from a JDBC connection pool
+        POOL.acquire();
+
+        // pretend to talk to a remote server
+        Thread.sleep(2 + RANDOM.nextInt((int)(data % 3)));
+
+        // pretend to care about the value
+        result.add(key.hashCode());
+        result.add(value.hashCode());
+        result.add(data);
+
+        // Put our thread back into the database.
+        POOL.release();
+    }
+}
---
+++ src/main/java/org/webtide/loom/ManyTasks.java
@@ -0,0 +1,109 @@
+package org.webtide.loom;
+
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.eclipse.jetty.util.thread.ExecutionStrategy;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.util.thread.strategy.EatWhatYouKill;
+import org.openjdk.jmh.infra.Blackhole;
+
+public class ManyTasks
+{
+    private static final Random RANDOM = new SecureRandom();
+
+    static Runnable newTask(CountDownLatch latch)
+    {
+        long id = RANDOM.nextLong();
+        String task = Integer.toHexString(RANDOM.nextInt());
+        return () ->
+        {
+            try
+            {
+                // pretend to authenticate the user
+                Object userdata = FakeDataBase.get(Long.toString(id));
+
+                // small chance auth fails.
+                if (userdata.toString().startsWith("6666"))
+                    return;
+
+                // pretend to get some app data
+                Object data = FakeDataBase.get(task);
+
+                // pretend to process the data
+                Blackhole.consumeCPU(1000 + data.hashCode() % 1000);
+
+                // pretend to mutate some app data
+                FakeDataBase.put(task, Long.toHexString(data.hashCode() + id).repeat(1 + Math.abs((int)(id % 10))));
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                // signal completion
+                latch.countDown();
+            }
+        };
+    }
+
+    private static void warmup(int tasks) throws InterruptedException
+    {
+        // warm up
+        CountDownLatch warmup = new CountDownLatch(tasks * 2);
+        for (int i = 0 ; i < tasks; i++)
+        {
+            Thread.builder().task(newTask(warmup)).start();
+            Thread.builder().virtual().task(newTask(warmup)).start();
+        }
+        warmup.await();
+    }
+
+    private static long testThreadPool(int tasks) throws Exception
+    {
+        CountDownLatch latch = new CountDownLatch(tasks);
+        QueuedThreadPool pool = new QueuedThreadPool(100, 100, -1, 0, null, null);
+        pool.start();
+
+        try
+        {
+            long started = System.nanoTime();
+            // run tasks with a thread pool
+            for (int i = 0; i < tasks; i++)
+                pool.execute(newTask(latch));
+            latch.await();
+            return System.nanoTime() - started;
+        }
+        finally
+        {
+            pool.stop();
+        }
+    }
+
+    private static long testVThreads(int tasks) throws Exception
+    {
+        CountDownLatch latch = new CountDownLatch(tasks);
+
+        long started = System.nanoTime();
+        // run tasks with a thread pool
+        for (int i = 0 ; i < tasks; i++)
+            Thread.builder().virtual().task(newTask(latch)).start();
+        latch.await();
+        return System.nanoTime() - started;
+    }
+
+    public static void main(String... args) throws Exception
+    {
+        warmup(20_000);
+
+        System.err.println("======");
+        System.err.printf("Pooled  K Threads %,dms%n", TimeUnit.NANOSECONDS.toMillis(testThreadPool(100_000)));
+        System.err.printf("Spawned V Threads %,dms%n", TimeUnit.NANOSECONDS.toMillis(testVThreads(100_000)));
+        System.err.println("======");
+        System.err.println(FakeDataBase.getResult());
+    }
+}
